cc85642dd8bbf39d163abde26231f7b592fa9e78
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-28 15:13:45 +0500

    change

--- src/premitiveDataTypes/floatetc.java
+++ src/premitiveDataTypes/floatetc.java
@@ -2,25 +2,14 @@ package src.premitiveDataTypes;
 
 public class floatetc {
     public static void main(String[]args){
-        double kilometers=75.114;
-        toMilesPerHour(kilometers);
-        System.out.println(Math.round(3.4));
-       // System.out.println("your total miles=" + toMilesPerHour(kilometers));
 
     }
-    public static void toMilesPerHour(double kiloMetersPerHour) {
-        if (kiloMetersPerHour > 0) {
-            long miles = Math.round( kiloMetersPerHour /1.6 );
-            System.out.println(kiloMetersPerHour+"k/h="+miles+"m/h");
-
-        } else if (kiloMetersPerHour <= 0) {
-            System.out.println("invalid value");
-        }
-
+    public static int teenchecker(int age1,int age2,int age3 ){
+        if(age1<13&&>)
     }
-
 
 
 }
+
 
 
