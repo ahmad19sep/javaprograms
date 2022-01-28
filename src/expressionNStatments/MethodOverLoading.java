e3016e55cdd82d1f091d0727502c868cef67142f
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-28 19:30:09 +0500

    SecToMinToHr

--- src/expressionNStatments/MethodOverLoading.java
+++ src/expressionNStatments/MethodOverLoading.java
@@ -1,30 +1,53 @@
 package src.expressionNStatments;
 
 public class MethodOverLoading {
-
     public static void main(String[] args) {
-       /* double feet = 0;
-        double inch = -8;
-        double inch1=9;
-        System.out.println("Total centimeter=" + feetToInch(feet, inch)+"cm");
-        System.out.println("Total centimeters in second function=" +feetToInch(inch1)+"cm");*/
+        int min=121;
+        int sec=43;
+        int sec1=3600;
+        sectomin(min,sec);
+        sectomin(sec1);
+
     }
-/*
-    public static double feetToInch(double feet, double inch) {
-        if (feet >= 0 && inch >= 0 && inch <= 12) {
-            double centi = (feet * 30.48) + (inch * 2.54);
-            return centi;
-        } else {
-            return -1;
-        }
-    }
-    public static double feetToInch(double inch){
-        if(inch>=0&&inch<12){
-            double centi=inch*2.54;
-            return centi;
+    public static int sectomin(int min,int sec){
+        if(min>=0&&sec>=0&&sec<59){
+            if(min>59){
+                int hour=min/60;
+                int min1=min-(hour*60);
+                System.out.println(hour+"h:"+min1+" m: "+sec+" s");
+            }
+            else if(min<=59){
+                int hour=0;
+                System.out.println(hour+"h: "+min+" m: "+sec+" s");
+            }
+
         }
         else {
-            return -1;
+            System.out.println("enter valid number");
         }
-    }*/
+        return 0;
+    }
+    public static int sectomin(int sec){
+        if(sec>=0){
+            if(sec>59){
+                int min=sec/60;
+                int sec1=(min*60)-sec;
+                if(min>59){
+                    int hour=min/60;
+                    int min1=min-(hour*60);
+                    System.out.println(hour+"h:"+min1+"m:"+sec1+"s");
+                }else {
+                    int hour=0;
+                    System.out.println(hour+"h:"+min+"m:"+sec1+"s");
+
+                }
+            }else {
+                int hour=0;
+                int min=0;
+                System.out.println(hour+"h:"+min+"m"+"s");
+            }
+        }else {
+            System.out.println("enter vald number");
+        }return 0;
+    }
 }
