2a47a4ec28bbd9290a9913374516dc8385c612e2
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-28 18:41:34 +0500

    centi

--- src/expressionNStatments/MethodOverLoading.java
+++ src/expressionNStatments/MethodOverLoading.java
@@ -3,13 +3,13 @@ package src.expressionNStatments;
 public class MethodOverLoading {
 
     public static void main(String[] args) {
-        double feet = 4;
-        double inch = 6;
+       /* double feet = 0;
+        double inch = -8;
         double inch1=9;
         System.out.println("Total centimeter=" + feetToInch(feet, inch)+"cm");
-        System.out.println("Total centimeters in second function=" +feetToInch(inch1)+"cm");
+        System.out.println("Total centimeters in second function=" +feetToInch(inch1)+"cm");*/
     }
-
+/*
     public static double feetToInch(double feet, double inch) {
         if (feet >= 0 && inch >= 0 && inch <= 12) {
             double centi = (feet * 30.48) + (inch * 2.54);
@@ -26,5 +26,5 @@ public class MethodOverLoading {
         else {
             return -1;
         }
-    }
+    }*/
 }
