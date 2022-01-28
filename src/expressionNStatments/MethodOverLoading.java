14878d9f538aabd4b44f9fce0c1d09583d44fdac
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-28 16:38:12 +0500

    int to centi

---
+++ src/expressionNStatments/MethodOverLoading.java
@@ -0,0 +1,30 @@
+package src.expressionNStatments;
+
+public class MethodOverLoading {
+
+    public static void main(String[] args) {
+        double feet = 4;
+        double inch = 6;
+        double inch1=9;
+        System.out.println("Total centimeter=" + feetToInch(feet, inch)+"cm");
+        System.out.println("Total centimeters in second function=" +feetToInch(inch1)+"cm");
+    }
+
+    public static double feetToInch(double feet, double inch) {
+        if (feet >= 0 && inch >= 0 && inch <= 12) {
+            double centi = (feet * 30.48) + (inch * 2.54);
+            return centi;
+        } else {
+            return -1;
+        }
+    }
+    public static double feetToInch(double inch){
+        if(inch>=0&&inch<12){
+            double centi=inch*2.54;
+            return centi;
+        }
+        else {
+            return -1;
+        }
+    }
+}
---
+++ src/expressionNStatments/floatetc.java
@@ -0,0 +1,25 @@
+package src.premitiveDataTypes;
+
+public class floatetc {
+    public static void main(String[] args) {
+
+        int num3 = 46;
+
+
+        System.out.println(teenchecker2(num3));
+    }
+
+    public static boolean teenchecker2(int num3) {
+        if (num3 > 13 && num3 < 19) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
+
+
+
