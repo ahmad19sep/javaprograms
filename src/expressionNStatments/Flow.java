8a325e7e6a816e4fc12245548a2532cc03e92022
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-29 23:25:45 +0500

    switch

---
+++ src/expressionNStatments/Flow.java
@@ -0,0 +1,21 @@
+package src.expressionNStatments;
+
+public class Flow {
+    public static void main(String[] args) {
+        int switchtest=17;
+        switch (switchtest){
+            case 1:
+                System.out.println("vale was 1");
+                break;
+            case 2:
+                System.out.println("value was 2");
+                break;
+            case 6:
+                System.out.println("value is 6");
+                break;
+            default:
+                System.out.println("enter valid number");
+                break;
+        }
+    }
+}
