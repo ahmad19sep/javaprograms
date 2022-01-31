e3aec1a75446ae0fec0cab3bd78749119eedf3f3
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-01-31 23:53:53 +0500

    days of the month

--- src/expressionNStatments/MethodOverLoading.java
+++ src/expressionNStatments/MethodOverLoading.java
@@ -2,52 +2,61 @@ package src.expressionNStatments;
 
 public class MethodOverLoading {
     public static void main(String[] args) {
-        int min=121;
-        int sec=43;
-        int sec1=3600;
-        sectomin(min,sec);
-        sectomin(sec1);
+        int year = 2009;
+        int month = 5;
 
+        System.out.println(monthyear(month,year));
     }
-    public static int sectomin(int min,int sec){
-        if(min>=0&&sec>=0&&sec<59){
-            if(min>59){
-                int hour=min/60;
-                int min1=min-(hour*60);
-                System.out.println(hour+"h:"+min1+" m: "+sec+" s");
-            }
-            else if(min<=59){
-                int hour=0;
-                System.out.println(hour+"h: "+min+" m: "+sec+" s");
-            }
 
-        }
-        else {
-            System.out.println("enter valid number");
-        }
-        return 0;
-    }
-    public static int sectomin(int sec){
-        if(sec>=0){
-            if(sec>59){
-                int min=sec/60;
-                int sec1=(min*60)-sec;
-                if(min>59){
-                    int hour=min/60;
-                    int min1=min-(hour*60);
-                    System.out.println(hour+"h:"+min1+"m:"+sec1+"s");
-                }else {
-                    int hour=0;
-                    System.out.println(hour+"h:"+min+"m:"+sec1+"s");
 
+
+    public static int monthyear(int month, int year) {
+        if (month > 1 && month < 12 && year > 0 && year < 9999) {
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+                switch (month) {
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                        return 31;
+
+                    case 2:
+                        return 29;
+                  //  break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        return 30;
+                    //break;
+                    default:
+                        return -1;
                 }
-            }else {
-                int hour=0;
-                int min=0;
-                System.out.println(hour+"h:"+min+"m"+"s");
+            } else {
+                switch (month) {
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                        return 31;
+                //    break;
+                    case 2:
+                        return 28;
+                  //  break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        return 30;
+                  //  break;
+                    default:
+                        return -1;
+                }
             }
-        }else {
-            System.out.println("enter vald number");
-        }return 0;
-    }
-}
+        }
+        else
+            return -1;
+    }}
+
