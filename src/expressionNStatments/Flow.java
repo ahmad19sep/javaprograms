7570a3b3444b406495b3fb7b1b2878dfb00258b8
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-02-01 21:10:08 +0500

    Ex:15 numPalindrome

--- src/expressionNStatments/Flow.java
+++ src/expressionNStatments/Flow.java
@@ -2,21 +2,24 @@ package src.expressionNStatments;
 
 public class Flow {
     public static void main(String[] args) {
-        int num=-125;
-        System.out.println("Your sum is "+sumDig(num));
-
+        int num=70707;
+        System.out.println(isPalindrome(num));
     }
-    private static int sumDig(int num){
-        if(num>=10){
-            int mod=0;
-            int div=num;
-            while (div!=0){
-                mod=mod+(div%10);
-                div=div/10;
-            }
-            return mod;
-        }else
-            return num;
+    public static boolean isPalindrome(int num){
+        int div=num;
+        int reverse=0;
+        while (div!=0){
+
+            reverse=reverse+(div%10);
+            div=(div/10);
+            reverse=reverse*10;
+        }
+        reverse=reverse/10;
+
+        if(reverse==num){
+            return true;
+        }
+        return false;
     }
 
 }
