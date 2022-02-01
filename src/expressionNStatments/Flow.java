05d12f36d1ac5a51a875df4c2b24404386a2a1da
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-02-01 17:45:03 +0500

    ex:14 sumOdd

--- src/expressionNStatments/Flow.java
+++ src/expressionNStatments/Flow.java
@@ -2,20 +2,35 @@ package src.expressionNStatments;
 
 public class Flow {
     public static void main(String[] args) {
-        int sum=0;
-        int count=0;
-        for(int i=1; i<=1000; i++){
-
-            if (i%3==0&&i%5==0){
-                System.out.println("number is"+i);
-                sum =sum+i;
-                count++;
-
-                if(count==5){
-                    break;
-                }
-            }
+        int num = -1;
+        int num1=13,num2=13;
+        if (isOdd(num)) {
+            System.out.println("YOUR NUMBER " + num + " IS ODD");
+        } else {
+            System.out.println("YOUR NUMBER " + num + " IS EVEN");
         }
-        System.out.println("Your sum of total five numbers is "+sum);
+        System.out.println(sumOdd(num1,num2));
+        System.out.println("Sum of odd numbers in this range is "+sumOdd(num1,num2));
+    }
+
+    public static boolean isOdd(int num) {
+        if ((num <= 0 || num % 2 == 0)) {
+            return false;
+        }
+        return true;
+    }
+    public static int sumOdd(int start, int end){
+        int sum=0;
+        if(start<=0||end<=0){
+            return -1;
+        }
+        else if(start>0&&end>0){
+            for(int i=start; i<=end; i++){
+                if(i%2!=0){
+                    sum=sum+i;
+                }
+
+            }
+        }return sum;
     }
 }
