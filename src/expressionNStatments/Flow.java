aa8b47513c6511738901aaf2df25fdddf5cd8770
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-02-04 19:32:22 +0500

    perfectNumber

--- src/expressionNStatments/Flow.java
+++ src/expressionNStatments/Flow.java
@@ -2,20 +2,26 @@ package src.expressionNStatments;
 
 public class Flow {
     public static void main(String[] args) {
-int num=56668;
-int num1=24684;
-        printFactor(num);
+int num=8128;
+
+        System.out.println(printNumber(num));
        }
-       public static void printFactor(int num){
+       public static boolean printNumber(int num){
        if(num>1){
-           for (int i=1; i<=num; i++){
+          int sum=0;
+           for (int i=1; i<num; i++){
                if(num%i==0){
-                   System.out.println(i+"\n");
+                  sum=sum+i;
                }
            }
+           if(sum==num){
+               return true;
+           }
+           else
+               return false;
        }
        else
-           System.out.println("invalid number");
+           return false;
 
        }
 }
