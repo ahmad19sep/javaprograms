a7fcee164bbf14c38c01507c2136b59f3087b445
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-02-04 17:49:52 +0500

    sharedDigit

--- src/expressionNStatments/Flow.java
+++ src/expressionNStatments/Flow.java
@@ -2,22 +2,25 @@ package src.expressionNStatments;
 
 public class Flow {
     public static void main(String[] args) {
-        int num=-1;
-        System.out.println (getEvenDigSum(num));
+        int num = 42;
+        int num1 = 43;
+        System.out.println(sharedDig(num, num1));
     }
-    public static int getEvenDigSum(int num){
-        if(num>0){
-            int mod=0,div=num,sum=0;
-            while (div!=0){
-                mod=div%10;
-                div=div/10;
-                if(mod%2==0){
-                    sum=sum+mod;
-                }
 
+    public static boolean sharedDig(int num, int num1) {
+        if (num > 10 && num1 > 10 && num1 < 99 && num < 99) {
+            int div = num;
+            int mod = 0;
+            mod = mod + div % 10;
+            div = div / 10;
+            int div1 = num1;
+            int mod1 = 0;
+            mod1 = mod1 + (div1 % 10);
+            div1 = div1 / 10;
+            if (mod == div1 || mod == mod1 || div == div1 || div == mod1) {
+                return true;
             }
-            return sum;
         }
-        return -1;
+        return false;
     }
 }
