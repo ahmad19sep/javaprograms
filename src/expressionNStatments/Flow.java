0e447d3128ae93a48434bf83b65f5c577bf9a7df
HP-Notebook <ahmadsiddiqueofficial1820665@gmail.com>
2022-02-05 09:07:48 +0500

    digitsToWords

--- src/expressionNStatments/Flow.java
+++ src/expressionNStatments/Flow.java
@@ -2,26 +2,64 @@ package src.expressionNStatments;
 
 public class Flow {
     public static void main(String[] args) {
-int num=8128;
+        int num = 30001;
+        numberToWords(num);
+    }
 
-        System.out.println(printNumber(num));
-       }
-       public static boolean printNumber(int num){
-       if(num>1){
-          int sum=0;
-           for (int i=1; i<num; i++){
-               if(num%i==0){
-                  sum=sum+i;
-               }
-           }
-           if(sum==num){
-               return true;
-           }
-           else
-               return false;
-       }
-       else
-           return false;
+    public static void numberToWords(int num) {
+        if (num % 10 != 0) {
+            int mod = 0;
+            String numToWords = "";
+            int orgNum = 0;
+            int div = num;
+            while (div != 0) {
+                mod = mod + (div % 10);
+                div = div / 10;
+                mod = mod * 10;
+            }
+            mod = mod / 10;
+            while (mod != 0) {
+                orgNum = (mod % 10);
+                switch (orgNum) {
+                    case 0:
+                        numToWords = numToWords + "zero ";
+                        break;
+                    case 1:
+                        numToWords = numToWords + "one ";
+                        break;
+                    case 2:
+                        numToWords = numToWords + "two ";
+                        break;
+                    case 3:
+                        numToWords = numToWords + "three ";
+                        break;
+                    case 4:
+                        numToWords = numToWords + "four ";
+                        break;
+                    case 5:
+                        numToWords = numToWords + "five ";
+                        break;
+                    case 6:
+                        numToWords = numToWords + "six ";
+                        break;
+                    case 7:
+                        numToWords = numToWords + "seven ";
+                        break;
+                    case 8:
+                        numToWords = numToWords + "eight ";
+                        break;
+                    case 9:
+                        numToWords = numToWords + "nine ";
+                        break;
+                }
+                mod = mod / 10;
 
-       }
+            }
+            System.out.println(num+" = "+numToWords);
+
+    }
 }
+}
+
+
+
